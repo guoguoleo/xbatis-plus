ce59d7e467c3c26327379102e0627869ed668f80
纸鸢～手中线 <a787761855@qq.com>
2026-01-23 17:21:09 +0800

    fix:增加注释

--- xbatis-plus-core/src/main/java/cn/xbatis/plus/GlobalGenerator.java
+++ xbatis-plus-core/src/main/java/cn/xbatis/plus/GlobalGenerator.java
@@ -13,21 +13,29 @@ public class GlobalGenerator {
 
     public ConfigurationCustomizer configurationCustomizer() {
         return configuration -> {
-
+            //region 加载自定义id
             GeneratorFactory.register(IdGeneratorConstant.snow, new SnowIdHelper());
             GeneratorFactory.register(IdGeneratorConstant.uuid, new UuidHelper());
             GeneratorFactory.register(IdGeneratorConstant.simpleUuid, new SimpleUuidGenerator());
-
-
+            //endregion
+            //插入数据监听
             XbatisGlobalConfig.setGlobalOnInsertListener(this::setGlobalOnInsertListener);
-
+            //更新数据监听
             XbatisGlobalConfig.setGlobalOnUpdateListener(this::setGlobalOnUpdateListener);
         };
     }
 
+    /**
+     * 全局插入数据监听
+     * @param o 实体对象
+     */
     public void setGlobalOnInsertListener(Object o) {
     }
 
+    /**
+     * 全局更新数据监听
+     * @param o 实体对象
+     */
     public void setGlobalOnUpdateListener(Object o) {
     }
 
